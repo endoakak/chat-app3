74ccc8952aacac091027805394e4694a69bd9fa1
endoakak <akendo2020@gmail.com>
2020-07-29 10:56:18 +0900

    新規作成画面の表示

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   root 'messages#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :users, only: [:edit, :update]
-  
+  resources :rooms, only: [:new, :create]
 
 end
